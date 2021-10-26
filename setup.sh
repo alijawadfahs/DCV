230a4841e166a066ad505d86d34aefe3b999bf1d
Ali_fahs <ali.fahs@activeeon.com>
2021-10-26 07:33:13 +0200

    restarting docker service

--- setup.sh
+++ setup.sh
@@ -1,11 +1,10 @@
 #!/bin/sh
-
 sudo yum update -y
-
 sudo amazon-linux-extras install docker -y
-
 sudo yum install docker -y
+
 
 sudo service docker start
 sudo usermod -a -G docker ec2-user
 sudo chmod 666 /var/run/docker.sock
+sudo service docker restart
