35567168475b51134c226f134ef037d0efc7145c
Ali_fahs <ali.fahs@activeeon.com>
2021-10-26 07:31:58 +0200

    adding the setup shell script

---
+++ setup.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+sudo yum update -y
+
+sudo amazon-linux-extras install docker -y
+
+sudo yum install docker -y
+
+sudo service docker start
+sudo usermod -a -G docker ec2-user
+sudo chmod 666 /var/run/docker.sock
